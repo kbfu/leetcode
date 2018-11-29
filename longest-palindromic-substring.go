e23e989b51aca39175f9135221cd6cec48d4f421
zhangdonghao <zhangdonghao@niwodai.net>
2018-11-30 01:36:55 +0800

    finish

--- longest-palindromic-substring.go
+++ longest-palindromic-substring.go
@@ -3,7 +3,6 @@
 package main
 
 import (
-	"bytes"
 	"fmt"
 )
 
@@ -13,38 +12,31 @@ func longestPalindrome(s string) string {
 		return s
 	}
 	for i := range s {
-		lastIndex := bytes.LastIndex([]byte(s), []byte{s[i]})
-		if lastIndex >= i {
-			str := s[i : lastIndex+1]
-			strLen := len(str)
-			if strLen%2 == 0 {
-				if str[0:strLen/2] == str[strLen/2:strLen] {
-					if strLen > len(palindrome) {
-						palindrome = str
-					}
-				}
-			} else {
-				midIndex := strLen / 2
-				if midIndex > 0 {
-					for i := 0; i < midIndex; i++ {
-						leftIndex := i
-						rightIndex := strLen - i - 1
-						if str[leftIndex] != str[rightIndex] {
-							break
-						}
-						if i == midIndex-1 && strLen > len(palindrome) {
-							palindrome = str
-						}
-					}
-				} else {
-					if strLen > len(palindrome) {
-						palindrome = str
-					}
-				}
-			}
+		str1 := find(s, i, i)
+		str2 := find(s, i, i+1)
+		if len(str1) > len(palindrome) {
+			palindrome = str1
+		}
+		if len(str2) > len(palindrome) {
+			palindrome = str2
 		}
 	}
 	return palindrome
+}
+
+func find(s string, left int, right int) string {
+	if right < len(s) && s[left] == s[right] {
+		for left > 0 && right < len(s)-1 {
+			if s[left-1] == s[right+1] {
+				left--
+				right++
+			} else {
+				break
+			}
+		}
+		return s[left : right+1]
+	}
+	return ""
 }
 
 func main() {
