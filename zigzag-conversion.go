b6f6622ca4fdc33e4b97bc82906055611ee54858
zhangdonghao <zhangdonghao@niwodai.net>
2018-12-02 22:36:30 +0800

    finish

---
+++ zigzag-conversion.go
@@ -0,0 +1,36 @@
+// https://leetcode-cn.com/problems/zigzag-conversion/description/
+
+package main
+
+import "fmt"
+
+func convert(s string, numRows int) string {
+	res := make([][]byte, numRows)
+	flag := -1
+	reversed := 0
+	joined := ""
+	for i := range s {
+		if reversed == 0 {
+			flag++
+		} else {
+			if flag > 0 {
+				flag--
+			}
+		}
+		if flag == numRows-1 {
+			reversed = 1
+		} else if flag == 0 {
+			reversed = 0
+		}
+		res[flag] = append(res[flag], s[i])
+	}
+
+	for i := range res {
+		joined = joined + string(res[i])
+	}
+	return joined
+}
+
+func main() {
+	fmt.Println(convert("AB", 1))
+}
