91e5dbf3b5c9b69bd6d5b2393c2c3165ea381c03
zhangdonghao <zhangdonghao@niwodai.net>
2018-12-02 23:19:47 +0800

    finish

---
+++ reverse-intege.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func reverse(x int) int {
+	rev := 0
+	for x != 0 {
+		pop := x % 10
+		x /= 10
+		if rev > math.MaxInt32/10 || (rev == math.MaxInt32/10 && pop > 7) {
+			return 0
+		}
+		if rev < math.MinInt32/10 || (rev == math.MinInt32/10 && pop < -8) {
+			return 0
+		}
+		rev = rev*10 + pop
+	}
+	return rev
+}
+
+func main() {
+	fmt.Println(reverse(7463847412))
+}
