605207abb633f99b09655a1047fb19988366ff50
zhangdonghao <zhangdonghao@niwodai.net>
2020-04-24 00:39:15 +0800

    better

--- string-to-integer-atoi.go
+++ string-to-integer-atoi.go
@@ -5,44 +5,64 @@ package main
 import (
 	"fmt"
 	"math"
-	"strconv"
 	"strings"
 )
 
 func myAtoi(str string) int {
 	str = strings.TrimSpace(str)
-	numStr := ""
+
 	if len(str) == 0 {
 		return 0
 	}
-	if str[0] == 45 || str[0] == 43 {
-		numStr += string(str[0])
-		for i := 1; i < len(str); i++ {
-			if str[i] <= 57 && str[i] >= 48 {
-				numStr += string(str[i])
-			} else {
-				break
-			}
-		}
-	} else if str[0] <= 57 && str[0] >= 48 {
-		for i := 0; i < len(str); i++ {
-			if str[i] <= 57 && str[i] >= 48 {
-				numStr += string(str[i])
-			} else {
-				break
-			}
-		}
-	} else {
+
+	result := 0
+	isNegative := false
+
+	// check negative & validate string
+	if !strings.Contains("1234567890-+", string(str[0])) {
 		return 0
 	}
-	num, _ := strconv.Atoi(numStr)
-	if num > math.MaxInt32 {
-		return math.MaxInt32
-	} else if num < math.MinInt32 {
-		return math.MinInt32
-	} else {
-		return num
+	if str[0] == '-' {
+		str = str[1:]
+		isNegative = true
+	} else if str[0] == '+' {
+		str = str[1:]
 	}
+
+	// check alphabet
+	for i := range str {
+		if str[i]-'0' < 0 || str[i]-'0' > 9 {
+			str = str[:i]
+			break
+		}
+	}
+
+	for i := 0; i < len(str); i++ {
+		if int(str[i]-'0') > 9 || int(str[i]-'0') < 0 {
+			break
+		}
+		if isNegative {
+			if float64(int(str[i]-'0'))*math.Pow10(len(str)-i-1) > float64(math.MinInt32)*-1 {
+				return math.MinInt32
+			}
+		} else {
+			if float64(int(str[i]-'0'))*math.Pow10(len(str)-i-1) > float64(math.MaxInt32) {
+				return math.MaxInt32
+			}
+		}
+		result += int(str[i]-'0') * int(math.Pow10(len(str)-i-1))
+	}
+
+	if isNegative {
+		result *= -1
+	}
+	if result < math.MinInt32 {
+		return math.MinInt32
+	} else if result > math.MaxInt32 {
+		return math.MaxInt32
+	}
+
+	return result
 }
 
 func main() {
